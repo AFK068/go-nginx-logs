ace257b8a193ed79e74411b6f9fabc83da73eaf3
ivan <6_6_6_66@maik.ru>
2024-11-17 21:18:50 +0300

    feat: :sparkles: build app in main

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/es-debug/backend-academy-2024-go-template/internal/application"
+)
+
 func main() {
-	// TODO: write your code here
+	initializer := &application.DefaultInizializer{}
+
+	appComponents, err := application.InitializeApp(initializer)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	dataProcessor, err := application.GetDataProcessor(appComponents.PathResult)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = dataProcessor.Process(appComponents.PathResult, &appComponents.LogReport)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	outputRenderer, err := application.GetOutputRenderer(appComponents.Config.Format)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	outputRenderer.Render(&appComponents.LogReport)
 }
