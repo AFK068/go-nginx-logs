38d62d362fb3bf002dae026d59fc3d15edc79d19
ivan <6_6_6_66@mail.ru>
2024-11-17 21:39:47 +0300

    feat: :sparkles: added loger initialize

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/es-debug/backend-academy-2024-go-template/internal/application"
+	"github.com/es-debug/backend-academy-2024-go-template/internal/infrastructure"
 )
 
 func main() {
+	logger, err := infrastructure.InitLogger()
+	if err != nil {
+		fmt.Println("failed to initialize logger:", err)
+		return
+	}
+
+	slog.SetDefault(logger.Logger)
+
+	defer func() {
+		if err := infrastructure.CloseLogger(logger); err != nil {
+			fmt.Println("failed to close logger:", err)
+		}
+	}()
+
 	initializer := &application.DefaultInizializer{}
 
 	appComponents, err := application.InitializeApp(initializer)
